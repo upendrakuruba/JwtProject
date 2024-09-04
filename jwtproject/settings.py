e3d9dd9e2062c628240616023b643b0258039f17
upendraupi <upendrakuruma@gmail.com>
2024-09-04 14:29:51 +0530

    cors2 commit

--- jwtproject/settings.py
+++ jwtproject/settings.py
@@ -186,10 +186,7 @@ USE_TZ = True
 
 
 CORS_ALLOWED_ORIGINS = [
-    "https://example.com",
-    "https://sub.example.com",
-    "http://localhost:8080",
-    "http://127.0.0.1:9000",
+    "http://localhost:8000"
 ]
 
 
