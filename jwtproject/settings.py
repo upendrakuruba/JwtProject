23237841074fa3e47e8dec2fd7740dfdefe09de9
upendraupi <upendrakuruma@gmail.com>
2024-09-04 14:24:18 +0530

    cors commit

--- jwtproject/settings.py
+++ jwtproject/settings.py
@@ -184,6 +184,15 @@ USE_I18N = True
 USE_TZ = True
 
 
+
+CORS_ALLOWED_ORIGINS = [
+    "https://example.com",
+    "https://sub.example.com",
+    "http://localhost:8080",
+    "http://127.0.0.1:9000",
+]
+
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
