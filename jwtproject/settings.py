e9830e681ce8bba06dd1dc4935f7af98d402c1e3
upendraupi <upendrakuruma@gmail.com>
2024-09-05 22:05:15 +0530

    mathu2commit

--- jwtproject/settings.py
+++ jwtproject/settings.py
@@ -32,16 +32,11 @@ ALLOWED_HOSTS = ["*"]
 
 
 CORS_ALLOWED_ORIGINS = [
-    'http://localhost:3000',  # Example for a local development serve
+    'http://localhost:8000',  # Example for a local development serve
 ]
 
-# settings.py
-SECURE_SSL_REDIRECT = True
 
 
-# settings.py
-SECURE_CONTENT_TYPE_NOSNIFF = True
-SECURE_BROWSER_XSS_FILTER = True
 
 # Application definition
 
