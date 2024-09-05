89488dab2e339884fe4ed4ed819917e645af2be3
upendraupi <upendrakuruma@gmail.com>
2024-09-05 22:10:57 +0530

    mathu2commit

--- jwtproject/settings.py
+++ jwtproject/settings.py
@@ -31,9 +31,11 @@ DEBUG = True
 ALLOWED_HOSTS = ["*"] 
 
 
-CORS_ALLOWED_ORIGINS = [
-    'http://localhost:8000',  # Example for a local development serve
-]
+# CORS_ALLOWED_ORIGINS = [
+#     'http://localhost:8000',  # Example for a local development serve
+# ]
+
+CORS_ALLOW_ALL_ORIGINS = True
 
 
 
