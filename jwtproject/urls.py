5d6ca828a8ff47b28447f02e6d0491a7b87d01b1
upendraupi <upendrakuruma@gmail.com>
2024-09-07 23:59:04 +0530

    changesdonetitle

--- jwtproject/urls.py
+++ jwtproject/urls.py
@@ -25,7 +25,7 @@ from drf_yasg import openapi
 
 schema_view = get_schema_view(
    openapi.Info(
-      title="INCOME EXPENSE API",
+      title="CURD AND AUTHENTICATION",
       default_version='v1',
       description="Test description",
       terms_of_service="//yourco/terms/",
